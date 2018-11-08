e5dbd66e839e5f7fc8ca3175a0bd184ba4879855
Geoff Bass <geoff.c.bass@gmail.com>
2018-11-08 12:00:04 -0500

    adds solution code and comments

--- index.js
+++ index.js
@@ -15,8 +15,16 @@ const mostFrequentWord = (text) => {
   return mostFrequentWord;
 }
 
-const findPassword = async () => {
-  // Your code goes here
+const findPassword = async () => { // all `async` functions return promises (need to be `await`ed)
+  const poem1 = await readFileAsync('./poems/starting-poem.txt', 'utf-8');
+  const poem2FileName = `poems/${mostFrequentWord(poem1)}.txt`;
+  const poem2 = await readFileAsync(`${poem2FileName}`, 'utf-8');
+  const poem3FileName = `poems/${mostFrequentWord(poem2)}.txt`;
+  const poem3 = await readFileAsync(`${poem3FileName}`, 'utf-8');
+  const password = mostFrequentWord(poem3);
+  return password; // this will be the resolved value of the promise `findPassword` returns (what we get when we `await` it)
 }
 
-findPassword();
+(async () => { // async IIFE allows us to await the result of findPassword
+  console.log('password:', await findPassword())
+})();
